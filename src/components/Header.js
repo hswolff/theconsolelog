5d5c370e7cbd013978d5bbc25fed1ab98d4469fe
Harry Wolff <hswolff@gmail.com>
2018-01-21 15:58:10 -0500

    feat: Style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,84 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import styled from 'react-emotion';
+import image from '../images/logo.png';
+
+const maxWidth = 960;
 
 export default () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
+  <header
+    css={`
+      background: #232121;
+      margin-bottom: 1.45rem;
+    `}
   >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          The Console Log
+    <HeaderInner>
+      <Left>
+        <Link to="/" css={{ margin: 'none', color: 'transparent' }}>
+          <Logo src={image} title="The Console Log" />
         </Link>
-      </h1>
-      <Link to="/episode/tags">Tags</Link>
-      <Link to="/about">About</Link>
-    </div>
-  </div>
+        <Title>
+          <Link to="/">The Console Log</Link>
+        </Title>
+      </Left>
+      <Right>
+        <Link to="/episode/tags">Tags</Link>
+        <Link to="/about">About</Link>
+      </Right>
+    </HeaderInner>
+  </header>
 );
+
+const HeaderInner = styled('div')`
+  max-width: ${maxWidth}px;
+  margin: 0 auto;
+  padding: 1.45rem 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Left = styled('div')`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const Logo = styled('img')`
+  width: 64px;
+  height: 64px;
+  margin: 0;
+  &,
+  &:hover {
+    text-decoration: none;
+  }
+`;
+
+const Title = styled('h1')`
+  font-family: 'American Typewriter', monospace, serif;
+  font-weight: normal;
+  margin: 0;
+  margin-left: 20px;
+  a {
+    box-shadow: none;
+    color: #fff;
+    &:hover {
+      box-shadow: inherit;
+      text-decoration: underline;
+    }
+  }
+`;
+
+const Right = styled('div')`
+  a {
+    color: #fff;
+    box-shadow: none;
+    & + a {
+      margin-left: 10px;
+    }
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -19,7 +19,7 @@ export default ({ children }) => (
       className={css({
         margin: '0 auto',
         maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
+        padding: '0px 0 1.45rem',
         paddingTop: 0,
       })}
     >
