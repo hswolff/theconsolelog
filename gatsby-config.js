73ed4e657af7db8927707743320f4280ec64a5ad
Matthew Gerstman <mattgerstman@gmail.com>
2019-01-30 08:33:10 -0800

    Fix prettier

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,42 @@
-const path = require("path");
-const { DateTime } = require("luxon");
+const path = require('path');
+const { DateTime } = require('luxon');
 
 module.exports = {
   siteMetadata: {
-    title: "The Console Log",
+    title: 'The Console Log',
     description:
-      "A weekly podcast about all the latest JavaScript and web news.",
-    siteUrl: "http://theconsolelog.com",
-    iTunesLogo: "https://theconsolelog.com/itunes-logo.jpeg",
+      'A weekly podcast about all the latest JavaScript and web news.',
+    siteUrl: 'http://theconsolelog.com',
+    iTunesLogo: 'https://theconsolelog.com/itunes-logo.jpeg',
   },
   plugins: [
-    "gatsby-plugin-react-next",
+    'gatsby-plugin-react-next',
     {
-      resolve: "gatsby-plugin-typography",
+      resolve: 'gatsby-plugin-typography',
       options: {
-        pathToConfigModule: "src/utils/typography.js",
+        pathToConfigModule: 'src/utils/typography.js',
       },
     },
-    "gatsby-plugin-emotion",
-    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: "UA-107877562-1",
+        trackingId: 'UA-107877562-1',
       },
     },
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-catch-links",
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-catch-links',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "episodes",
-        path: path.join(__dirname, "episodes"),
+        name: 'episodes',
+        path: path.join(__dirname, 'episodes'),
       },
     },
-    "gatsby-transformer-json",
+    'gatsby-transformer-json',
     {
-      resolve: "gatsby-plugin-feed",
+      resolve: 'gatsby-plugin-feed',
       options: {
         query: `
         {
@@ -61,41 +61,41 @@ module.exports = {
             ...siteMetadata,
             ...rest,
             image_url: siteMetadata.iTunesLogo,
-            language: "en",
+            language: 'en',
             custom_namespaces: {
-              itunes: "http://www.itunes.com/dtds/podcast-1.0.dtd",
+              itunes: 'http://www.itunes.com/dtds/podcast-1.0.dtd',
             },
             custom_elements: [
-              { "itunes:subtitle": siteMetadata.description },
-              { "itunes:author": "The Console Log" },
+              { 'itunes:subtitle': siteMetadata.description },
+              { 'itunes:author': 'The Console Log' },
               {
-                "itunes:summary": siteMetadata.description,
+                'itunes:summary': siteMetadata.description,
               },
               {
-                "itunes:owner": [
-                  { "itunes:name": "The Console Log" },
-                  { "itunes:email": "hello@hswolff.com" },
+                'itunes:owner': [
+                  { 'itunes:name': 'The Console Log' },
+                  { 'itunes:email': 'hello@hswolff.com' },
                 ],
               },
-              { "itunes:explicit": "no" },
+              { 'itunes:explicit': 'no' },
               {
-                "itunes:image": {
+                'itunes:image': {
                   _attr: {
                     href: siteMetadata.iTunesLogo,
                   },
                 },
               },
               {
-                "itunes:category": [
+                'itunes:category': [
                   {
                     _attr: {
-                      text: "Technology",
+                      text: 'Technology',
                     },
                   },
                   {
-                    "itunes:category": {
+                    'itunes:category': {
                       _attr: {
-                        text: "Tech News",
+                        text: 'Tech News',
                       },
                     },
                   },
@@ -153,51 +153,52 @@ module.exports = {
                     url: createPodcastUrl(node.fields.episodeNumber),
                   },
                   custom_elements: [
-                    { "itunes:author": "The Console Log" },
-                    { "itunes:subtitle": node.title },
+                    { 'itunes:author': 'The Console Log' },
+                    { 'itunes:subtitle': node.title },
                     {
-                      "itunes:image": {
+                      'itunes:image': {
                         _attr: {
                           href: siteMetadata.iTunesLogo,
                         },
                       },
                     },
-                    { "itunes:duration": node.duration || "00:00" },
-                    { "itunes:summary": createDescription(node) },
+                    { 'itunes:duration': node.duration || '00:00' },
+                    { 'itunes:summary': createDescription(node) },
                   ],
                 });
               });
             },
-            output: "/rss.xml",
+            output: '/rss.xml',
           },
         ],
       },
     },
     {
-      resolve: "gatsby-plugin-offline",
+      resolve: 'gatsby-plugin-offline',
     },
   ],
 };
 
-const podcastUrlBase = "https://s3.amazonaws.com/the-console-log-podcast";
+const podcastUrlBase =
+  'https://chtbl.com/track/E72DG/https://s3.amazonaws.com/the-console-log-podcast';
 const createPodcastUrl = input => {
   const episodeNumber = String(input).length === 1 ? `0${input}` : input;
   return `${podcastUrlBase}/E${episodeNumber}.mp3`;
 };
 
 function formatDate(date) {
-  return DateTime.fromISO(date).toFormat("LLLL d");
+  return DateTime.fromISO(date).toFormat('LLLL d');
 }
 
 function getYear(date) {
-  return DateTime.fromISO(date).toFormat("y");
+  return DateTime.fromISO(date).toFormat('y');
 }
 
 const createDescription = node => {
   let description = `
 Episode ${node.fields.episodeNumber}.
 ${formatDate(node.date.start)} - ${formatDate(node.date.end)}, ${getYear(
-    node.date.end,
+    node.date.end
   )}
 
 FIND ALL LINKS ON THE WEBSITE
@@ -214,9 +215,9 @@ SHOW NOTES
   node.content.forEach(({ name, links }) => {
     description += `\n# ${name}\n`;
 
-    description += links.join("\n");
+    description += links.join('\n');
 
-    description += "\n";
+    description += '\n';
   });
 
   return description;
